02676187c614015a2d2c8851f38dd67e945cd60c
Константин Абашин <kabashin@mail.ru>
2022-10-07 10:46:02 +0300

    настройка deploy v2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -61,6 +61,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 # Database
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
 
 DATABASES = {
     'default': {
